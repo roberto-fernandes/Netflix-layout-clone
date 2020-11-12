d245af7484247444e3a3880143235602bc47950e
Roberto Fernandes <roberto.fernandes21@gmail.com>
2020-11-12 12:22:14 +0000

    Initial Commit

---
+++ Netflix layout clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix layout clone
+//
+//  Created by Roberto Fernandes on 12/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
